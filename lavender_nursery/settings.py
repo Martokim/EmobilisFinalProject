b2191e54833eb37de2b79b86064bfe2d97f3def2
Martin <martinkimani42@gmail.com>
2024-12-10 11:23:50 +0300

    .env file configuration

--- lavender_nursery/settings.py
+++ lavender_nursery/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,7 +142,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'fsnforce@gmail.com'  
-EMAIL_HOST_PASSWORD =  'uqplubkqeposnfdr' 
+EMAIL_HOST_USER = config('SMTP_USER') 
+EMAIL_HOST_PASSWORD =  config('SMTP_PASSWORD')
 
 
